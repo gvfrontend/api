373393323b97f931c3faa60f3b9bf876354d89f9
gvfrontend <gk.2023.krish@gmail.com>
2024-12-25 23:11:56 +0530

    routes

--- main.js
+++ main.js
@@ -9,7 +9,26 @@ app.get("/" , (req, res)=>{
     res.json({msg: "Hello students"})
 })
 
+//getting the movies
+app.get("/movies",()=>{
 
+}
+)
+//posting the movies
+app.post("/movies",()=>{
+    
+}
+)
+//updating the movie
+app.put("/movies/:id",()=>{
+    
+}
+)
+
+//deleting the movie
+app.delete("/movies/:id", ()=>{
+
+})
 
 app.listen(PORT, ()=>{
     console.log(` The sever is running at http://localhost:${PORT}`)
